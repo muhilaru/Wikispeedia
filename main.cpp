110478bc43a956f1f272c360c04ed69c9b8040de
Muhil Arumugam <muhila2@linux-a2.ews.illinois.edu>
2020-12-04 15:57:47 -0600

    Created input flow with Interpreter in main

--- main.cpp
+++ main.cpp
@@ -9,10 +9,24 @@ int main(int argc, char *argv[]) {
     argc = 0;
     argv = nullptr;
 
-    std::cout << "UGHHHHHHHH!!!!" << std::endl;
+    std::cout << "Welcome to WikiSpeedia! Enter start to begin your game." << std::endl;
 
+    string input;
+    
+    bool init = true;
 
+    Interpreter interpreter;
 
+    while (getline(std::cin, input) && input != "exit") {
+
+      if (init) {
+        interpreter.processStartInput(input);
+        init = false;
+      } else {
+        interpreter.processGameInput(input);
+      }
+
+    }
 
   return 0;
 }
