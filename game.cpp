83919fcbc709d15dd4a9c97e4c60c640d088c369
Aidan Sochowski <aidants2@linux-a1.ews.illinois.edu>
2020-12-04 00:39:51 -0600

    Filled out all functions'

--- game.cpp
+++ game.cpp
@@ -1,1 +1,75 @@
 #include "game.h"
+
+//================================================================================
+// Public functions.
+//================================================================================
+
+// Creates game from random beginning and end.
+Game::Game() {
+
+};
+
+// Creates game from predefined start and end.
+Game::Game(Vertex start, Vertex end) {
+
+};
+
+// uses existing matrix to calculate shortest path between start and end using Floyd
+void Game::calculateShortestPath() {
+
+};
+
+// Create matrix based on graph’s current state using Floyd Warshall’s Algorithm
+void Game::generateMatrix() {
+
+};
+
+// Reads in generated adjacency matrix
+void Game::readAdjacencyMatrix(string matrix_path) {
+
+};
+
+// Adds a vertex to the graph
+void Game::addPage(Vertex vert) {
+
+};
+
+// Adds an edge to the graph
+void Game::addLink(Vertex from, Vertex to) {
+
+};
+
+// Returns a list of all of the 
+std::vector<Vertex> Game::getValidPaths() {
+    return std::vector<Vertex>();
+};
+
+// Use iterative deepening DFS to find the most optimal path.
+std::vector<Edge> Game::getOptimiumPath() {
+    return std::vector<Edge>();
+};
+
+// Attempt to move to a specific page. Return true if moved to valid page.
+bool Game::moveTo(Vertex to) {
+    return false;
+};
+
+// Returns true if a vertex was popped off the stack
+bool Game::moveBack() {
+    return false;
+};
+
+// Returns true if at the final node
+bool Game::isComplete() {
+    return false;
+};
+
+// Returns true if at the beginning node
+bool Game::isAtStart() {
+    return false;
+};
+
+//================================================================================
+// Private functions.
+//================================================================================
+
--- game.h
+++ game.h
@@ -1,9 +1,46 @@
 #pragma once
 
+#include "graph.h"
+#include "edge.h"
+#include <stack>
+#include <vector>
+
+/**
+// The Game class keeps track of the user's movement throughout the graph, allowing them to traverse 
+// to different Wikipedia pages in the graph. Also can generate an adjacency matrix for other games to use.
+**/
 class Game {
     public:
+        // Creates game from random beginning and end.
         Game();
-        
-    private:
-
+        // Creates game from predefined start and end.
+        Game(Vertex start, Vertex end);
+        // uses existing matrix to calculate shortest path between start and end using Floyd
+        void calculateShortestPath();
+        // Create matrix based on graph’s current state using Floyd Warshall’s Algorithm
+        void generateMatrix();
+        // Reads in generated adjacency matrix
+        void readAdjacencyMatrix(string matrix_path);
+        // Adds a vertex to the graph
+        void addPage(Vertex vert);
+        // Adds an edge to the graph
+        void addLink(Vertex from, Vertex to);
+        // Returns a list of all of the 
+        std::vector<Vertex> getValidPaths();
+        // Use iterative deepening DFS to find the most optimal path.
+        std::vector<Edge> getOptimiumPath();
+        // Attempt to move to a specific page. Return true if moved to valid page.
+        bool moveTo(Vertex to);
+        // Returns true if a vertex was popped off the stack
+        bool moveBack();
+        // Returns true if at the final node
+        bool isComplete();
+        // Returns true if at the beginning node
+        bool isAtStart();
+	private:
+		Vertex start_;
+		Vertex end_;
+		Vertex current_;
+		Graph* graph_;
+		std::stack<Vertex> path_taken_;
 };
