f98d267ea9f6e152534876b37248d8f32cd9a499
Chandrachur Kesana <ckesan2@linux-a2.ews.illinois.edu>
2020-12-06 14:25:42 -0600

    Finished writing tests for generateMatrix() method

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -10,11 +10,37 @@ TEST_CASE("THICCCCCOOOOO BAMBA") {
   REQUIRE( true == true);
 }
 
+TEST_CASE("generateMatrix Simple") {
+
+  Game game("Color", "Leprechaun");
+
+  game.addPage("Color");
+  game.addPage("Rainbow");
+  game.addPage("Leprechaun");
+
+  game.addLink("Color", "Rainbow");
+  game.addLink("Rainbow", "Leprechaun");
+
+  std::vector<Vertex> articles = game.getArticles();
+  Graph graph = game.getGraph();
+
+  const int INF = 1e7;
+
+  std::vector<std::vector<int>> expected {
+
+    {0, 1, 2},
+    {INF, 0, 1},
+    {INF, INF, 0}
+  };
+
+  std::vector<std::vector<int>> actual = game.generateMatrix(graph, articles);
+
+  REQUIRE(actual == expected);
+}
+
 TEST_CASE("generateMatrix Basic Case") {
 
   Game game("Color", "Leprechaun");
-  std::cout << "Hello!!" << std::endl;
-
 
   game.addPage("Chan");
   game.addPage("Color");
@@ -22,24 +48,16 @@ TEST_CASE("generateMatrix Basic Case") {
   game.addPage("Rainbow");
   game.addPage("Leprechaun");
 
-  std::cout << "ADDED PAGES" << std::endl;
-
   game.addLink("Chan", "Forest");
   game.addLink("Chan", "Leprechaun");
   game.addLink("Chan", "Color");
   game.addLink("Color", "Rainbow");
   game.addLink("Rainbow", "Leprechaun");
 
-  std::cout << "ADDED LINKS" << std::endl;
-
   std::vector<Vertex> articles = game.getArticles();
   Graph graph = game.getGraph();
 
   const int INF = 1e7;
-
-  for (int i = 0; i < (int)articles.size(); i++) {
-    std::cout<< articles.at(i) << std::endl;
-  }
 
   std::vector<std::vector<int>> expected {
 
@@ -52,7 +70,42 @@ TEST_CASE("generateMatrix Basic Case") {
 
   std::vector<std::vector<int>> actual = game.generateMatrix(graph, articles);
 
+  REQUIRE(actual == expected);
+}
+
+TEST_CASE("generateMatrix Complex Cycles Case") {
+
+  Game game("Blue", "Purple");
+
+  game.addPage("Blue");
+  game.addPage("Red");
+  game.addPage("Green");
+  game.addPage("Purple");
+
+  game.addLink("Blue", "Red");
+  game.addLink("Red", "Blue");
+
+  game.addLink("Blue", "Green");
+
+  game.addLink("Blue", "Purple");
+  game.addLink("Purple", "Blue");
+
+  game.addLink("Red", "Green");
+  game.addLink("Red", "Purple");
+  game.addLink("Green", "Purple");
+
+  std::vector<Vertex> articles = game.getArticles();
+  Graph graph = game.getGraph();
+
+  std::vector<std::vector<int>> expected {
+
+    {0, 1, 1, 1},
+    {1, 0, 1, 1},
+    {2, 3, 0, 1},
+    {1, 2, 2, 0}
+  };
+
+  std::vector<std::vector<int>> actual = game.generateMatrix(graph, articles);
 
   REQUIRE(actual == expected);
-
 }
