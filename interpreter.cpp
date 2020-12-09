fe365d581cd9b2ab0375521d9fb9a95dc68fa403
Aidan Sochowski <aidants2@linux-a1.ews.illinois.edu>
2020-12-08 21:01:06 -0600

    Now reads in from dataset

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,4 +1,7 @@
 #include "interpreter.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
 
 //================================================================================
 // Public functions.
@@ -13,6 +16,7 @@ Interpreter::Interpreter() {
 std::string Interpreter::processStartInput(std::string input) {
     if (input == "start") {
         createGame(MIN_GAME_SIZE);
+        readFromDataset("data/articles.tsv", "data/links.tsv")
         return "Created game.";
     } else {
         return COMMAND_INVALID;
@@ -25,9 +29,27 @@ std::string Interpreter::processGameInput(std::string input) {
 };
 
 // Add all vertices and edges from dataset into game/graph.
-//need to take in articles.tsv and links.tsv so I added one extra param
+// need to take in articles.tsv and links.tsv so I added one extra param
 std::string Interpreter::readFromDataset(std::string articles_path, std::string links_path) {
-
+    std::ifstream articles(articles_path);
+    std::string line;
+    while (std::getline(articles, line)) {
+        if (line.size() > 0 && line[0] != '#') {
+            game_->addPage(line);
+        }
+    }
+    std::ifstream links(links_path);
+    while (std::getline(links, line)) {
+        if (line.size() > 0 && line[0] != '#') {
+            std::istringstream line_stream(line);
+            std::vector<std::string> pages;
+            std::string page;
+            while(std::getline(line_stream, page, '\t')) {
+                pages.push_back(page);
+            }
+            game_->addLink(pages[0], pages[1]);
+        }
+    }
     return COMMAND_INVALID;
 };
 
@@ -37,24 +59,16 @@ std::string Interpreter::readFromDataset(std::string articles_path, std::string 
 
 // Create random game with maximum path length
 void Interpreter::createGame(int length) {
-
-    Game game;
-    game_ = &game;
-    game_->setMax(length);
-
+    game_ = new Game();
 };
 
 // Create game starting at a specific title and ending at a specific title
 void Interpreter::createGame(Vertex start, Vertex end) {
-
-    // Game game(start, end);
-    // game_ = &game;
-
+    game_ = new Game(start, end);
 };
 
 // Prints out valid paths to console
 std::string Interpreter::getValidPathsStr() {
-
     string list_of_paths = "";
     for (unsigned k = 0; k < game_->getValidPaths().size(); k++) {
         list_of_paths += game_->getValidPaths()[k];
